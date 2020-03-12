fbb88b922608f915c9a738b3feadb39b9dd7a5e7
Michael O'Keefe <okeefem355@gmail.com>
2020-03-12 00:00:34 -0400

    Need those api keys...

---
+++ src/config/config.ts
@@ -0,0 +1,11 @@
+export const FIREBASE_CONFIG = {
+    apiKey: 'AIzaSyDuFmMawOI77xuTnLcttDt_atpUUyQ0HG8',
+    authDomain: 'something-cool-84a9b.firebaseapp.com',
+    databaseURL: 'https://something-cool-84a9b.firebaseio.com',
+    projectId: 'something-cool-84a9b',
+    storageBucket: 'something-cool-84a9b.appspot.com',
+    messagingSenderId: '866612540999',
+    appId: '1:866612540999:web:66465c9d7318217264a362',
+};
+
+export const GOOGLE_MAPS_API_KEY = 'AIzaSyDf-79O8qJv6hra1nrSIPJrty1eijP8r48';
