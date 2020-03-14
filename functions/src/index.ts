8c5fcea4fe1a7113ef112c741416361d744460d4
Michael O'Keefe <okeefem355@gmail.com>
2020-03-14 02:09:09 -0400

    Setting up functions with sendgrid

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,26 @@
 import * as functions from 'firebase-functions';
+import * as sgMail from '@sendgrid/mail';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.rsvp = functions.firestore
+    .document('invitations/{invitationId}')
+    .onUpdate(change => {
+        const after = change.after.data();
+        const before = change.before.data();
+        if (!before || !after) return;
+        const beforeRsvp = before.rsvp;
+        const afterRsvp = after.rsvp;
+        if (afterRsvp !== beforeRsvp && !!afterRsvp) {
+            const invitees = after.names.join(' and ');
+            sgMail.setApiKey(functions.config().sendgrid.key);
+            const msg = {
+                to: 'okeefem355@gmail.com',
+                from: 'receptioneering-functions@receptioneering.com',
+                subject: 'Receptioneering Rsvp Updated',
+                text: `${invitees} have updated their rsvp status to ${afterRsvp}${
+                    after.note ? `with note ${after.note}` : ''
+                }`,
+            };
+            return sgMail.send(msg);
+        }
+        return;
+    });
